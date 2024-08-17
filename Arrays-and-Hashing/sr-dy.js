788f29bd8a44b2e336d832140772f43970c5bc56
haseeb2412 <haseeb241202@gmail.com>
2024-08-17 14:58:54 +0500

    solve 1 to 3 question for logic building

---
+++ Arrays-and-Hashing/sr-dy.js
@@ -0,0 +1,31 @@
+
+// static array fixed in size 
+// console.log("haseeb here  new file");
+
+// problem 01 ( contains duplicate ) -----------------------------------------------------------------------
+
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// let num = [1,2,3,1];
+// 
+// for (let i = 0; i < num.length; i++) {
+    // let s = new Set([...num]);
+    //  return s.size !== num.length;
+// 
+// }
+
+
+// problem 02 ( valid anagaram )-----------------------------------------------
+
