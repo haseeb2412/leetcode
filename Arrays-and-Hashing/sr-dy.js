6144bf3be4032803c67abf15b928a225f6388c6f
haseeb2412 <haseeb241202@gmail.com>
2024-08-17 15:37:05 +0500

    solve 2 more questions

--- Arrays-and-Hashing/sr-dy.js
+++ Arrays-and-Hashing/sr-dy.js
@@ -18,14 +18,73 @@
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
-// let num = [1,2,3,1];
-// 
-// for (let i = 0; i < num.length; i++) {
-    // let s = new Set([...num]);
-    //  return s.size !== num.length;
-// 
-// }
+let num = [1,2,3,1];
+
+for (let i = 0; i < num.length; i++) {
+    let s = new Set([...num]);
+     return s.size !== num.length;
+
+}
 
 
 // problem 02 ( valid anagaram )-----------------------------------------------
 
+
+// Example 1:
+// 
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+// 
+// Input: s = "rat", t = "car"
+// Output: false
+// 
+
+let s = "rat"; let t = "car";
+
+let ss = s.split("").sort().join("");
+let tt = t.split("").sort().join("");
+if (ss == tt) {
+    console.log('return true');
+}
+else{
+    console.log("return false");
+}
+console.log("ss",ss);
+console.log("tt",tt);
+
+// problem 03 ( valid anagaram )-----------------------------------------------
+
+
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+let nums = [3,3], target = 6;
+let answer=[];
+for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i]+nums[j] == target) {
+            if(answer.includes(i) || answer.includes(j)){
+                continue;
+            }else{
+                answer.push(i,j);
+
+            }
+        }
+    }
+    
+}
+
+console.log("answer",answer);
