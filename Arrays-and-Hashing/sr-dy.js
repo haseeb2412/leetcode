1454029a82b8af9ff1be1163057788a617818ba1
haseeb2412 <haseeb241202@gmail.com>
2024-08-19 17:09:05 +0500

    solve one more problem

--- Arrays-and-Hashing/sr-dy.js
+++ Arrays-and-Hashing/sr-dy.js
@@ -18,13 +18,13 @@
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
-let num = [1,2,3,1];
+// let num = [1,2,3,1];
 
-for (let i = 0; i < num.length; i++) {
-    let s = new Set([...num]);
-     return s.size !== num.length;
+// for (let i = 0; i < num.length; i++) {
+//     let s = new Set([...num]);
+//      return s.size !== num.length;
 
-}
+// }
 
 
 // problem 02 ( valid anagaram )-----------------------------------------------
@@ -40,18 +40,18 @@ for (let i = 0; i < num.length; i++) {
 // Output: false
 // 
 
-let s = "rat"; let t = "car";
+// let s = "rat"; let t = "car";
 
-let ss = s.split("").sort().join("");
-let tt = t.split("").sort().join("");
-if (ss == tt) {
-    console.log('return true');
-}
-else{
-    console.log("return false");
-}
-console.log("ss",ss);
-console.log("tt",tt);
+// let ss = s.split("").sort().join("");
+// let tt = t.split("").sort().join("");
+// if (ss == tt) {
+//     console.log('return true');
+// }
+// else{
+//     console.log("return false");
+// }
+// console.log("ss",ss);
+// console.log("tt",tt);
 
 // problem 03 ( valid anagaram )-----------------------------------------------
 
@@ -71,23 +71,61 @@ console.log("tt",tt);
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-let nums = [3,3], target = 6;
-let answer=[];
-for (let i = 0; i < nums.length; i++) {
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[i]+nums[j] == target) {
-            if(answer.includes(i) || answer.includes(j)){
-                continue;
-            }else{
-                answer.push(i,j);
+// let nums = [3,3], target = 6;
+// let answer=[];
+// for (let i = 0; i < nums.length; i++) {
+//     for (let j = 1; j < nums.length; j++) {
+//         if (nums[i]+nums[j] == target) {
+//             if(answer.includes(i) || answer.includes(j)){
+//                 continue;
+//             }else{
+//                 answer.push(i,j);
 
-            }
-        }
-    }
+//             }
+//         }
+//     }
     
-}
+// }
 
-console.log("answer",answer);
+// console.log("answer",answer);
 
 
 // problem 04 ( Group Anagram )----------------------------
+
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
+
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+let strs = ["eat","tea","tan","ate","nat","bat"];
+let sorted = strs.map((str)=>str.split("").sort().join(""));
+console.log(sorted);
+let map={};
+
+for (let i = 0; i < sorted.length; i++) {
+    if(!map[sorted[i]]){
+        map[sorted[i]] = [strs[i]];
+    }
+    else{
+        map[sorted[i]].push(strs[i])
+
+    }
+
+    
+}
+return Object.values(map);
+// for (let i = 0; i < strs.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+        
+        
+//     }
+// }
