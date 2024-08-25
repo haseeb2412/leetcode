d134c34fc2de606e773f2caefc2c12bd03708ac8
haseeb2412 <haseeb241202@gmail.com>
2024-08-25 16:56:25 +0500

    solve one problem

--- two-pointer/problems.js
+++ two-pointer/problems.js
@@ -254,14 +254,24 @@
 // Input: s = "([])"
 
 // Output: true
+let s = "()[]{}";
 
-s = "(";
-for (let i = 0; i < s.length; i++) {
-    if (s[i]=='(' && s[i+1]==")") {
-        console.log("true")
-    }else{
-        console.log("false")
-    }
+const stack = [];
     
+for (let i = 0 ; i < s.length ; i++) {
+    let c = s.charAt(i);
+    switch(c) {
+        case '(': stack.push(')');
+            break;
+        case '[': stack.push(']');
+            break;
+        case '{': stack.push('}');
+            break;
+        default:
+            if (c !== stack.pop()) {
+                return false;
+            }
+    }
 }
 
+return stack.length === 0;
