2825a65cea137de7a8628a09db195c21f3c87fe8
haseeb2412 <haseeb241202@gmail.com>
2024-08-25 23:22:21 +0500

    binary serach problem

--- two-pointer/problems.js
+++ two-pointer/problems.js
@@ -254,24 +254,79 @@
 // Input: s = "([])"
 
 // Output: true
-let s = "()[]{}";
+// let s = "()[]{}";
 
-const stack = [];
+// const stack = [];
     
-for (let i = 0 ; i < s.length ; i++) {
-    let c = s.charAt(i);
-    switch(c) {
-        case '(': stack.push(')');
-            break;
-        case '[': stack.push(']');
-            break;
-        case '{': stack.push('}');
-            break;
-        default:
-            if (c !== stack.pop()) {
-                return false;
-            }
-    }
-}
+// for (let i = 0 ; i < s.length ; i++) {
+//     let c = s.charAt(i);
+//     switch(c) {
+//         case '(': stack.push(')');
+//             break;
+//         case '[': stack.push(']');
+//             break;
+//         case '{': stack.push('}');
+//             break;
+//         default:
+//             if (c !== stack.pop()) {
+//                 return false;
+//             }
+//     }
+// }
 
-return stack.length === 0;
+// return stack.length === 0;
+
+
+// problem ------------------------ binary search ------------------------
+
+// Example 1:
+
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+// Example 2:
+
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+
+
+
+let nums = [-1,0,3,5,9,12], target = 9;
+let = ans=[];
+let left =0 ,right = nums.length-1;
+while (left <= right) {
+    let mid =Math.floor((left+right)/2);
+    if (nums[mid]===target) {
+        ans.push(mid);
+    }else if(nums[mid] < target){
+        left =mid+1;
+    }else{
+        right = mid -1;
+    }
+    return -1;
+}
+console.log(ans)
+
+
+
+// this is the correct code for binary search 
+var search = function(nums, target) {
+    let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (nums[mid] === target) {
+          return mid;
+      } else if (nums[mid] < target) {
+          left = mid + 1;
+      } else {
+          right = mid - 1;
+      }
+  }
+
+  return -1;
+};
+
