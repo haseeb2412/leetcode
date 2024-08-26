a2d1a78807c1cc5999c88c88a712eab84cbd4566
haseeb2412 <haseeb241202@gmail.com>
2024-08-26 18:10:37 +0500

    solved min stack

---
+++ Stack/problems.js
@@ -0,0 +1,73 @@
+// problem 01 -------------------Min Stack ---------------------
+
+// Input
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// Output
+// [null,null,null,null,-3,null,0,-2]
+
+// Explanation
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin(); // return -3
+// minStack.pop();
+// minStack.top();    // return 0
+// minStack.getMin(); // return -2
+
+
+
+var MinStack = function() {
+    this.stack =[];
+    this.min = Infinity;
+    this.size =0;
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    if(val < this.min){
+        this.min =val;
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    const poped =this.stack.pop();
+    if(this.min ==  poped){
+        this.min =Math.min(...this.stack);
+    }
+
+    return poped;
+    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length-1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.min;
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
