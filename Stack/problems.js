f868553f7d65d3b933fb61dc7a9415ad0e03724c
haseeb2412 <haseeb241202@gmail.com>
2024-08-28 21:11:04 +0500

    evaluate polish notation solved

--- Stack/problems.js
+++ Stack/problems.js
@@ -104,3 +104,44 @@ var generateParenthesis = function(n) {
     dfs ("",0,0)
     return ans
 };
+
+
+
+
+// problem --------- evaluate reverse polish notation
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const stack = [];
+
+    const isOperator = (str) => {
+        return str === "+" || str === "-" || str === "*" || str === "/";
+    };
+
+    for (const token of tokens) {
+        if (isOperator(token)) {
+            const ele2 = parseInt(stack.pop());
+            const ele1 = parseInt(stack.pop());
+            let result = 0;
+
+            if (token === "+") {
+                result = ele1 + ele2;
+            } else if (token === "-") {
+                result = ele1 - ele2;
+            } else if (token === "*") {
+                result = ele1 * ele2;
+            } else if (token === "/") {
+                result = Math.trunc(ele1 / ele2); // Ensure integer division
+            }
+
+            stack.push(result.toString());
+        } else {
+            stack.push(token);
+        }
+    }
+
+    return parseInt(stack.pop());
+};
