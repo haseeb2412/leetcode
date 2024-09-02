5c1f39fd1f7cbda85888ea512cf485e3b92e55c7
haseeb2412 <haseeb241202@gmail.com>
2024-09-03 01:09:50 +0500

    answer comment

--- two-pointer/problems.js
+++ two-pointer/problems.js
@@ -311,24 +311,24 @@ console.log(ans)
 
 
 // this is the correct code for binary search 
-var search = function(nums, target) {
-    let left = 0;
-  let right = nums.length - 1;
+// var search = function(nums, target) {
+//     let left = 0;
+//   let right = nums.length - 1;
 
-  while (left <= right) {
-      const mid = Math.floor((left + right) / 2);
+//   while (left <= right) {
+//       const mid = Math.floor((left + right) / 2);
 
-      if (nums[mid] === target) {
-          return mid;
-      } else if (nums[mid] < target) {
-          left = mid + 1;
-      } else {
-          right = mid - 1;
-      }
-  }
+//       if (nums[mid] === target) {
+//           return mid;
+//       } else if (nums[mid] < target) {
+//           left = mid + 1;
+//       } else {
+//           right = mid - 1;
+//       }
+//   }
 
-  return -1;
-};
+//   return -1;
+// };
 
 
 
